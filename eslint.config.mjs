28111b731548999f143aca3a2ac0b845d97c69cf
calogero_messina <c.messina97engineer@gmail.com>
2025-01-24 14:33:50 +0100

    create repo

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  {
+    plugins: {
+      prettier: prettierPlugin, // Aggiungi il plugin Prettier
+    },
+    rules: {
+      'prettier/prettier': 'error', // Abilita le regole di Prettier come errori
+    },
+  },
+  prettierConfig,
+];
+
+export default eslintConfig;
